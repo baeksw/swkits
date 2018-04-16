0ee13f41956131e56bc3b28b681baae4207b385b
BaekSeungWoo <dohaskell7@gmail.com>
2018-04-16 16:06:54 +0900

    remove calendarapp

--- calendar_project/bms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from calendarapp import views
-from rest_framework import routers
-
-
-urlpatterns = [
-    #   path('', include(router.urls)),
-]
